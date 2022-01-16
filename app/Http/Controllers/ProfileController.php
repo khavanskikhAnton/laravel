72ccf2eaa6d288b8ccf26fdc043e7391695a9241
khavanskikhAnton <khavanskikh.a@mail.ru>
2022-01-17 01:29:23 +0500

    1

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ProfileController extends Controller
+{
+    public function profile($id)
+    {
+        $user = User::findOrFail($id);
+        return view('profile', compact('user'));
+    }
+
+    public function save()
+    {
+
+        request()->validate([
+            'name' => 'required',
+            'email' => 'email|required|unique:users',
+        ]);
+
+        $input = request()->all();
+        $name = $input['name'];
+        $email = $input['email'];
+        $userId = $input['userId'];
+
+        $user = User::find($userId);
+        $user->name = $name;
+        $user->email = $email;
+        $user->save();
+        return back();
+    }
+}
