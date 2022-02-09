bb44ccf8868f71020c6521663656a8b42f3c7b8f
khavanskikhAnton <khavanskikh.a@mail.ru>
2022-02-09 21:00:51 +0500

    1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -15,10 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
-
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::prefix('admin')->middleware(['auth', 'CheckIsAdmin'])->group(function() {
     Route::get('/', [App\Http\Controllers\AdminController::class, 'admin'])->name('admin');
@@ -26,19 +24,23 @@ Route::prefix('admin')->middleware(['auth', 'CheckIsAdmin'])->group(function() {
     Route::get('/products', [App\Http\Controllers\AdminController::class, 'products'])->name('adminProducts');
     Route::get('/categories', [App\Http\Controllers\AdminController::class, 'categories'])->name('adminCategories');
     Route::get('/enterAsUser/{id}', [App\Http\Controllers\AdminController::class, 'enterAsUser'])->name('enterAsUser');
-    Route::get('/enterAsProduct/{id}', [App\Http\Controllers\AdminController::class, 'enterAsProduct'])->name('enterAsProduct');
+    
     
 });
 
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 
 Route::get('/profile/{id}', [App\Http\Controllers\ProfileController::class, 'profile'])->name('profile');
-
+Route::get('/category/{category}', [App\Http\Controllers\HomeController::class, 'category'])->name('category');
 Route::post('/profile/save', [App\Http\Controllers\ProfileController::class, 'save'])->name('saveProfile');
 
 Route::get('/profileProduct/{id}', [App\Http\Controllers\ProfileProductController::class, 'profileProduct'])->name('profileProduct');
 
 Route::post('/profileProduct/save', [App\Http\Controllers\ProfileProductController::class, 'save'])->name('saveProfileProduct');
+
+Route::get('/profileCategory/{id}', [App\Http\Controllers\ProfileCategoryController::class, 'profileCategory'])->name('profileCategory');
+
+Route::post('/profileCategory/save', [App\Http\Controllers\ProfileCategoryController::class, 'save'])->name('saveProfileCategory');
