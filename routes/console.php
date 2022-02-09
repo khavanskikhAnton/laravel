daca0f4ae1482b3cb00c43da359e415746214052
khavanskikhAnton <khavanskikh.a@mail.ru>
2022-02-09 21:00:56 +0500

    1

--- routes/console.php
+++ routes/console.php
@@ -16,6 +16,29 @@ use Illuminate\Support\Facades\Artisan;
 | simple approach to interacting with each command's IO methods.
 |
 */
+Artisan::command('importCategoriesFromFile', function(){
+
+    $file = fopen('categories.csv', 'r');
+
+    $i = 0;
+    $insert = [];
+    while ($row = fgetcsv($file, 1000, ';')) {
+        
+        if ($i++ == 0) {
+            $bom = pack('H*','EFBBBF');
+            $row = preg_replace("/^$bom/", '', $row);
+            $columns = $row;
+            continue;
+        }
+
+        $data = array_combine($columns, $row);
+        $data['created_at'] = date('Y-m-d H:i:s');
+        $data['updated_at'] = date('Y-m-d H:i:s');
+        $insert[] = $data;
+    }
+
+    Category::insert($insert);
+});
 
 Artisan::command('parseEcatalog', function(){
     $url = 'https://www.e-katalog.ru/list/122/pr-1358/';
@@ -23,11 +46,21 @@ Artisan::command('parseEcatalog', function(){
     $data = file_get_contents($url);
 
     $dom = new DOMDocument();
-
     @$dom->loadHTML($data);
+    
     $xpath = new DomXPath($dom);
+    $totalProductsString = $xpath->query("//span[@class='t-g-q]")[0]->nodeValue ?? false;
+
+    $output = preg_match_all('/\d+/', $totalProductsString, $matches);
+
+    $totalProducts = (int) $matches[0][0];
 
     $divs = $xpath->query("//div[@class='model-short-div list-item--goods   ']");
+
+    $productsOnOnePage = $divs->length;
+    $pages = ceil($totalProducts / $productsOnOnePage);
+
+    $products = [];
 
     foreach ($divs as $div) {
         $a = $xpath->query("descendant::a[@class='model-short-title no-u']", $div);
@@ -35,9 +68,60 @@ Artisan::command('parseEcatalog', function(){
         
         $price = 0;
         $ranges = $xpath->query("descendant::div[@class='model-price-range']", $div);
-            if ($ranges->length == 1) {
-                $price = $ranges[0]->nodeValue;                
+
+        if ($ranges->length == 1) {
+            foreach ($ranges[0]->childNodes as $child) {
+                if ($child->nodeName == 'a') {
+                $price = 'от' . $child->nodeValue;
+                }
             }
+        }
+        $ranges = $xpath->query("descendant::div[@class='pr31 ib']", $div);
+        if ($ranges->length == 1) {
+            $price = $ranges[0]->nodeValue;
+        }
+        $products[] = [
+            'name' => $name,
+            'price' => $price,
+        ];
+    }
+
+    for ($i=1; $i < $pages; $i++) {
+        $nextUrl = "$url&page_=$i";
+        $data = file_get_contents($nextUrl);
+        $dom = new DOMDocument();
+        @$dom->loadHTML($data);
+    
+        $xpath = new DomXPath($dom);
+        $divs = $xpath->query("//div[@class='model-short-div list-item--goods   ']");
+        foreach ($divs as $div) {
+            $a = $xpath->query("descendant::a[@class='model-short-title no-u']", $div);
+            $name = $a[0]->nodeValue;
+            
+            $price = 0;
+            $ranges = $xpath->query("descendant::div[@class='model-price-range']", $div);
+    
+            if ($ranges->length == 1) {
+                foreach ($ranges[0]->childNodes as $child) {
+                    if ($child->nodeName == 'a') {
+                    $price = 'от' . $child->nodeValue;
+                    }
+                }
+            }
+            $ranges = $xpath->query("descendant::div[@class='pr31 ib']", $div);
+            if ($ranges->length == 1) {
+                $price = $ranges[0]->nodeValue;
+            }
+            $products[] = [
+                'name' => $name,
+                'price' => $price,
+            ];
+        }
+    }
+    $file = fopen('videocards.csv', 'w');
+    foreach ($products as $product) {
+        fputcsv($file, $product, ';');
+        fclose($file);
     }
 });
 
@@ -109,7 +193,6 @@ Artisan::command('updateProduct', function() {
         'name' => 'Процессоры'
     ]);
 });
-
 
 Artisan::command('deleteProduct', function() {
     
